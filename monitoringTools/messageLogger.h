2b26813b47a2ae282a1e6f4f01c5fc968fede86b
YoungWoo93 <rmstjd23@gmail.com>
2023-05-26 23:52:36 +0900

    testMain 내용 각 헤더에 example 추가

--- monitoringTools/messageLogger.h
+++ monitoringTools/messageLogger.h
@@ -5,6 +5,16 @@
 
 #include "logger/logger.h"
 
+#define LOG(...)                            writeReq(strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__, __LINE__, __VA_ARGS__);
+
+
+#define LOGOUT(level, mode)                 do { logLevel __level = level; char __MODE = mode;  std::string __FILENAME = ""; std::stringstream __ss; __ss
+#define LOGOUT_EX(level, mode, fileName)    do {logLevel __level = level; char __MODE = mode;  std::string __FILENAME = fileName; std::stringstream __ss; __ss
+
+#define LOGEND                              ""; writeReq(strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__, __LINE__, __level, __MODE, __ss.str(), __FILENAME); } while(0)
+
+
+
 void writeReq(const std::string& _logCppName,
 	const int _line,
 	const logLevel _level,
@@ -13,11 +23,19 @@ void writeReq(const std::string& _logCppName,
 	const std::string _logWriteFileName = "");
 
 
-
-#define LOG(...)                            writeReq(strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__, __LINE__, __VA_ARGS__);
-
-
-#define LOGOUT(level, mode)                 do { logLevel __level = level; char __MODE = mode;  std::string __FILENAME = ""; std::stringstream __ss; __ss
-#define LOGOUT_EX(level, mode, fileName)    do {logLevel __level = level; char __MODE = mode;  std::string __FILENAME = fileName; std::stringstream __ss; __ss
-
-#define LOGEND                              ""; writeReq(strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__, __LINE__, __level, __MODE, __ss.str(), __FILENAME); } while(0)
+/////////////////////////////////////////////////////////////
+// example
+/////////////////////////////////////////////////////////////
+//
+//	#include "../monitoringTools/messageLogger.h"
+//	
+//	void main()
+//	{
+//		LOG(logLevel::Info, LO_TXT | LO_CMD, "test info log out text, cmd");
+//		LOGOUT(logLevel::Info, LO_TXT | LO_CMD) << "test info log out text, cmd " << "by stream" << LOGEND;
+//		LOGOUT_EX(logLevel::Info, LO_TXT | LO_CMD, "extraFile") << "test info log out text, cmd " << "by stream " << "to extra.txt file" << LOGEND;
+//	
+//		Sleep(100); //로깅 이벤트가 전달되기 전에 로깅 스레드가 종료되는것을 지연시키기 위한 sleep
+//	}
+//
+/////////////////////////////////////////////////////////////
--- monitoringTools/performanceProfiler.h
+++ monitoringTools/performanceProfiler.h
@@ -2,4 +2,44 @@
 
 #include "profiler/profiler.h"
 #include "profiler/scopeProfiler.h"
-
+															
+/////////////////////////////////////////////////////////////
+// example
+/////////////////////////////////////////////////////////////
+//
+//	#include <thread>
+//	#include "monitoringTools/performanceProfiler.h"
+//	
+//	void testFunc1()
+//	{
+//		for (int i = 0; i < 100; i++)
+//		{
+//			performanceProfiler::startProfile("test1");
+//			for (int j = 0; j < 10000; j++) {
+//				j++;
+//			}
+//			performanceProfiler::endProfile("test1");
+//		}
+//	}
+//	
+//	void testFunc2()
+//	{
+//		for (int i = 0; i < 100; i++)
+//		{
+//			scopeProfiler a("test2");
+//			for (int j = 0; j < 10000; j++) {
+//				j++;
+//			}
+//		}
+//	}
+// 
+//	void main()
+//	{
+//		std::thread t1(testFunc1);
+//		std::thread t2(testFunc2);
+//	
+//		t1.join();
+//		t2.join();
+//	}
+//
+/////////////////////////////////////////////////////////////
