dc8cd2d574ee561b475957ab963f4701a9e29df2
YoungWoo93 <rmstjd23@gmail.com>
2023-05-28 16:46:18 +0900

    resurceMonitor 예제 추가

--- monitoringTools/resourceMonitor.h
+++ monitoringTools/resourceMonitor.h
@@ -23,3 +23,50 @@ private:
 	memoryMonitoring _memory;
 	CpuMonitoring* _cpu;
 };
+
+/////////////////////////////////////////////////////////////
+// example
+/////////////////////////////////////////////////////////////
+//
+//	
+//	#include <iostream>
+//	#include "../monitoringTools/resourceMonitor.h"
+//	
+//	using namespace std;
+//	
+//	void testFunc1(std::string arg)
+//	{
+//		monitoringCurrentThread(arg);
+//	
+//		int i = 0;
+//		for (int i = 0; i < 1000000000; i++)
+//			Sleep(100);
+//	}
+//	
+//	void testFunc2(std::string arg)
+//	{
+//		monitoringCurrentThread(arg);
+//	
+//		for (int i = 0; i < 10000000; i--)
+//			i++;
+//	}
+//	
+//	void main()
+//	{
+//		resourceMonitor testPDH;
+//		thread t1(testFunc1, "sleep");
+//		thread t2(testFunc2, "add");
+//	
+//		while (true)
+//		{
+//			Sleep(100);
+//	
+//			cout << testPDH.getAllCPURate()->total << endl;
+//			cout << testPDH.getUserMemorySize() << endl << endl;
+//			for (auto it : *testPDH.getThreadsCPURate()) {
+//				cout << "\t" << it.first << "\t:\t" << it.second.total << endl;
+//			}
+//		}
+//	}
+//
+/////////////////////////////////////////////////////////////
